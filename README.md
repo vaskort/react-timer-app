96e1286dfb605ac59041b014bf62344f120ef8b0
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-04-21 19:40:30 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# react-playground
+This project was created from a Udemy course to learn ReactJS.
+
+Check it [here](http://agile-island-96878.herokuapp.com/).
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Navigation: 'app/components/Navigation.jsx',
+      applicationStyles: 'app/styles/app.scss'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'inline-source-map'
+};
